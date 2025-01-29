4912c9c491cc7f15ff6effa8ce1deb610c24cc6a
Oyinlola Ojelere <ojelere.oyinlola@yahoo.com>
2025-01-29 12:55:35 +0100

    completed tests

--- LogixGoDesktop/src/utils/Helper.java
+++ LogixGoDesktop/src/utils/Helper.java
@@ -79,7 +79,7 @@ public class Helper {
 
 	public static boolean isNumeric(String str) {
 		try {
-			Integer.parseInt(str);
+			Long.parseLong(str);
 			return true;
 		} catch (NumberFormatException e) {
 			return false;
